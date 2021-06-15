638344b74071c1069218e38e60db03f4cbfe4c88
Yaochuang <ych_ding@163.com>
2021-06-15 12:18:43 +0800

    update shaders

--- PN_Triangles/shaders/Tessellation.vs.glsl
+++ PN_Triangles/shaders/Tessellation.vs.glsl
@@ -8,8 +8,7 @@ struct V2T
 };
 
 layout(location = 0) in vec4 vertexPosition_modelspace;
-layout(location = 1) in vec4 vertexColor;
-layout(location = 2) in vec3 vertexNormal_modelspace;
+layout(location = 1) in vec3 vertexNormal_modelspace;
 
 out V2T vdata;
 
