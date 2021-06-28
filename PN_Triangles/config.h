cce39aeb926eca405e9d089cc579b8700bb5fee4
Yaochuang <ych_ding@163.com>
2021-06-28 22:47:34 +0800

    temp.

--- PN_Triangles/config.h
+++ PN_Triangles/config.h
@@ -7,15 +7,14 @@
 #include <glm/glm.hpp>
 
 
-
 struct RenderSetting
 {
     // the input of current setting
-    int  onlyUseForIndentification;
-    int  width;
-    int  height;
-    int  bvhBuilderType;
-    bool enableTess;
+    int  width = 1280;
+    int  height = 720;
+    bool enableTess{false};
+    glm::vec4 outerTessLevel{2.f};
+    glm::vec3 innerTessLevel{1.f};
 };
 
 struct DisplayOption
